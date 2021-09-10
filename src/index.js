b91805a71755200ae3ed583308a50f93467a99a0
+ <+>
2021-09-09 20:44:02 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -17,54 +17,54 @@ fetch(imgUrl)
 function renderDogs(dog) {
     const image = document.createElement('img') //create image elements
     image.src = dog; // set source attribute of image to links in array
-    const container = document.getElementById('dog-image-container')
-    container.append(image) 
+    const container = document.getElementById('dog-image-container') //grab container element
+    container.append(image) // append all the images to the container
 }
 
 // Deliverable 2
-fetch(breedUrl)
+fetch(breedUrl) //fetch data from second URL
 .then(function(resp) {
-    return resp.json()
+    return resp.json() //convert it to json
 })
 .then(function (breedData) {
-    Object.keys(breedData.message).forEach(renderBreeds)
-    breeds = Object.keys(breedData.message)
+    Object.keys(breedData.message).forEach(renderBreeds) //collect all keys from breedData.message obj and pass each of them in renderBreeds
+    breeds = Object.keys(breedData.message) // fill empty array (breeds) with all keys from line above
 })
 
 
 
  function renderBreeds(breed) {
-    const list = document.createElement('li')
-    list.innerHTML = breed
+    const list = document.createElement('li') //create list element for dog breeds to go on
+    list.innerHTML = breed // set text content of each list element to a single breed
     //console.log(breed)
-    const breedList = document.getElementById('dog-breeds')
-    breedList.append(list)
+    const breedList = document.getElementById('dog-breeds') // grab the unordered list element
+    breedList.append(list) // append the breed list to the ul
     // Deliverable 3
-    list.addEventListener('click', function() {
-        if (list.style.color === 'pink') {
+    list.addEventListener('click', function() { //add event listener to the list so it can change colors
+        if (list.style.color === 'pink') { //when the list is clicked: if the color is already pink, turn it back to black
             list.style.color = 'black'
         }
-        else {
-            list.style.color = 'pink'
+        else { //if the text color is black
+            list.style.color = 'pink' //change it to pink
         }
     })
  }
 
  // Deliverable 4
- const dropDown = document.getElementById('breed-dropdown')
+ const dropDown = document.getElementById('breed-dropdown') // grab drop-down element
  dropDown.addEventListener('change', sortBreeds) //SELECT (DROP-DOWN) = "CHANGE" , not "click"
 
  function sortBreeds(e) {
-   let letter =  e.target.value // a, b, c, d
-   let result = breeds.filter(breed => breed.charAt(0) === letter) // array with breed names that start with "letter"
+   let letter =  e.target.value // a, b, c, d in drop down
+   let result = breeds.filter(breed => breed.charAt(0) === letter) // array with breed names that start with first letter = a/b/c/d
         while (document.querySelector('ul').firstChild) {
-            document.querySelector('ul').removeChild(document.querySelector('ul').firstChild)
+            document.querySelector('ul').removeChild(document.querySelector('ul').firstChild) //removes old list and replaces it with filtered one
         }
-   let filteredList = result.forEach(function(dog) {
-       const newList = document.createElement('li')
-       newList.innerHTML = dog
-       const letterBreedList = document.getElementById('dog-breeds')
-       letterBreedList.append(newList)
+   let filteredList = result.forEach(function(dog) { //filtered list is array with names that start with a/b/c/d and then passed into function
+       const newList = document.createElement('li') //create list element again
+       newList.innerHTML = dog //each element is a dog breed that begins with corresponding letter
+       const letterBreedList = document.getElementById('dog-breeds') //grab ul element
+       letterBreedList.append(newList) //append the new list to the ul
         
    })
      
